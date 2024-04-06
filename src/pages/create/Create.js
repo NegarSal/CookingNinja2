987f2cecb4832e61746ad886ea608b4caa7df651
Negar <negarsaljooghi@gmail.com>
2024-04-06 15:56:05 +0330

    Adding Multiple Ingredients

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 
 // styles
 import './Create.css'
@@ -7,10 +7,23 @@ export default function Create() {
   const [title, setTitle] = useState('')
   const [method, setMethod] = useState('')
   const [cookingTime, setCookingTime] = useState('')
+  const [newIngredient, setNewIngredient] = useState('')
+  const [ingredients, setIngredients] = useState([])
+  const ingredientInput = useRef(null)
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(title, method, cookingTime)
+    console.log(title, method, cookingTime, ingredients)
+  }
+  const handleAdd = (e) => {
+    e.preventDefault()
+    const ing = newIngredient.trim()
+    
+    if (ing && !ingredients.includes(ing)) {
+      setIngredients(prevIngredients => [...prevIngredients, ing])
+    }
+    setNewIngredient('')
+    ingredientInput.current.focus()
   }
 
   return (
@@ -34,6 +47,18 @@ export default function Create() {
             value={method}
             required
           />
+        </label>
+        <label>
+          <span>Recipe ingredients:</span>
+          <div className='ingredients'>
+            <input
+              type='text'
+              onChange={(e) => setNewIngredient(e.target.value)}
+              value={newIngredient}
+              ref={ingredientInput}
+            />
+            <button onClick={handleAdd} className='btn'>add</button>
+          </div>
         </label>
         <label>
           <span>cooking time (minutes):</span>
