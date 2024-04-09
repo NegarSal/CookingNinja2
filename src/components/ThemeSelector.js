ce24b6e6f645c2a897ad037cf9c2047cb8686312
Negar <negarsaljooghi@gmail.com>
2024-04-09 16:36:27 +0330

    Light & Dark Mode Selector

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,4 +1,5 @@
 import { useTheme } from '../hooks/useTheme'
+import modeIcon from '../assets/mode-icon.svg'
 
 // styles
 import './ThemeSelector.css'
@@ -6,10 +7,22 @@ import './ThemeSelector.css'
 const themecolors = ['#58249c', '#249c6b', '#b70233']
 
 export default function ThemeSelector() {
-  const { changeColor } = useTheme()
+  const { changeColor, changeMode, mode } = useTheme()
+
+  const toggleMode = () => {
+    changeMode(mode === 'dark' ? 'light' : 'dark')
+  }
+  console.log(mode)
 
   return (
     <div className='theme-selector'>
+      <div className='mode-toggle'>
+        <img
+          onClick={toggleMode}
+          src={modeIcon}
+          alt='modeIcon'
+        />
+      </div>
       <div className='theme-buttons'>
         {themecolors.map(color => (
           <div
